877e01bc65adfdd24284ad32af40b05f73bcd1b7
Yvonne Muriuki <yvonnemuriuki01@gmail.com>
2024-05-14 23:48:14 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+  <div>
+    <h1>Hi, I'm Yvonne</h1>
+    <img alt='author' src='http://fakesource.com/' />
+    <h2>About Me</h2>
+    <p>This is my bio.</p>
+    <a href='http://linkedin.com'>LinkedIn</a>
+    <br/>
+    <a href='http://github.com'>GitHub</a> 
+  </div>
+  )
 }
 
 export default App;
